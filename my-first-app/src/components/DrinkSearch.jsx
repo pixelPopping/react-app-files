607f29c2c687b77c9936a36efc8ac437dec9b283
Yorian Fransz <sydney-cook@outlook,com>
2024-06-07 22:50:08 +0200

    change

--- my-first-app/src/components/DrinkSearch.jsx
+++ my-first-app/src/components/DrinkSearch.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { TextInput } from './ui/TextInput';
+import { TextInput } from './ui/Textinput';
 
 export const DrinkSearch = () => {
 	const [searchField, setSearchField] = useState('test drink');
