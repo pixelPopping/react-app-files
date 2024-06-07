13e3d3f4f80b7a0abc77a8df2d5f30f71e90efdf
Yorian Fransz <sydney-cook@outlook,com>
2024-06-07 18:04:47 +0200

    add some code

--- my-first-app/src/components/DrinkChoice.jsx
+++ my-first-app/src/components/DrinkChoice.jsx
@@ -2,6 +2,7 @@
 export const DrinkChoice = ({drink}) => {
 return (
     <>
+    <h2>Your choice: {drink.name}</h2>
     <img src={drink.imgUrl} alt={drink.alt} width="100px"
     height="100px" />
     <p>Your drink will be ready soon</p>
