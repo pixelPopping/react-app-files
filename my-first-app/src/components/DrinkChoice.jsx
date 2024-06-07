a08e357c3a9ed4166764b77c5f34b3aba3d0a522
Yorian Fransz <sydney-cook@outlook,com>
2024-06-07 17:51:50 +0200

    remove and add some coded

--- my-first-app/src/components/DrinkChoice.jsx
+++ my-first-app/src/components/DrinkChoice.jsx
@@ -2,7 +2,7 @@
 export const DrinkChoice = ({drink}) => {
 return (
     <>
-    <p>{drink.name}</p>
+    {userDrink && <DrinkChoice drink={userDrink} />}
     <img src={drink.imgUrl} alt={drink.alt} width="100px"
     height="100px" />
     <p>Your drink will be ready soon</p>
