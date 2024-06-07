28d9b9e95c2922e5ac04528519993ed738c46bce
Yorian Fransz <sydney-cook@outlook,com>
2024-06-07 17:47:40 +0200

    change script

--- my-first-app/src/App.jsx
+++ my-first-app/src/App.jsx
@@ -6,13 +6,14 @@ import {DrinkChoice} from './components/DrinkChoice';
 
 export const App = () => {
 	const greeting = 'Welcome to our cafe!';
-	{userDrink && <DrinkChoice drink ={userDrink} />}
+	const userDrink = undefined;
 
 	return (
 		<div className="app">
 			<h1>{greeting}</h1>
 			<DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} />
 			<DrinkChoice drink={userDrink} />
+			{userDrink && <DrinkChoice drink={userDrink} />}
 		</div>
 	);
 };
