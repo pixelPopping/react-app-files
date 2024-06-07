8f72f763744b1db4720687e02a6deea23ac4fc03
Yorian Fransz <sydney-cook@outlook,com>
2024-06-07 20:33:46 +0200

    add code

--- my-first-app/src/components/DrinkChoice.jsx
+++ my-first-app/src/components/DrinkChoice.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 
 export const DrinkChoice = ({drink}) => {
+const [userDrink, setUserDrink] = useState();
 return (
     <>
     <h2>Your choice: {drink.name}</h2>
