a1ecffd164f20d3fe99d67b1b53fc030cee80b19
Yorian Fransz <sydney-cook@outlook,com>
2024-06-07 21:15:59 +0200

    remove

--- my-first-app/src/components/DrinkChoice.jsx
+++ my-first-app/src/components/DrinkChoice.jsx
@@ -1,7 +1,4 @@
-import { useState } from "react";
-
 export const DrinkChoice = ({drink}) => {
-useState
 return (
     <>
     <h2>Your choice: {drink.name}</h2>
@@ -10,4 +7,4 @@ return (
     <p>Your drink will be ready soon</p>
     </>
 );
-};q
+};
