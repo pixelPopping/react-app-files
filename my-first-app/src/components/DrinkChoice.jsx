3f7b115b3f5bcca943a38022c367ec72d1c8d236
Yorian Fransz <sydney-cook@outlook,com>
2024-06-07 17:57:09 +0200

    remove code

--- my-first-app/src/components/DrinkChoice.jsx
+++ my-first-app/src/components/DrinkChoice.jsx
@@ -2,7 +2,6 @@
 export const DrinkChoice = ({drink}) => {
 return (
     <>
-    {userDrink && <DrinkChoice drink={userDrink} />}
     <img src={drink.imgUrl} alt={drink.alt} width="100px"
     height="100px" />
     <p>Your drink will be ready soon</p>
