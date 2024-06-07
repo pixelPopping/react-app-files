f652f95476268146822da9701c8d6720dc05231a
Yorian Fransz <sydney-cook@outlook,com>
2024-06-07 18:41:09 +0200

    add greeting

--- my-first-app/src/App.jsx
+++ my-first-app/src/App.jsx
@@ -7,12 +7,10 @@ import {DrinkChoice} from './components/DrinkChoice';
 export const App = () => {
 	const greeting = 'Welcome to our cafe!';
 	const userDrink = undefined;
-	const DrinkSearch = coffee || tea ;
 
 	return (
 		<div className="app">
-			< h1 className={userDrink ? "smallHeader" : "bigHeader"}>{greeting}</h1>
-			<DrinkSearch availableDrinks={availableDrinks}/>
+            <h1>{greeting}</h1>
 			<DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} />
 			{userDrink ? (<DrinkChoice drink={userDrink} />): 'Please select a drink'}
 		</div>
