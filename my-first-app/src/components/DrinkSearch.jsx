3a3ff06e35fb9edf547b038acf28877a22b54378
Yorian Fransz <sydney-cook@outlook,com>
2024-06-08 21:57:52 +0200

    comit

--- my-first-app/src/components/DrinkSearch.jsx
+++ my-first-app/src/components/DrinkSearch.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { TextInput } from './ui/TextInput'; // Remove the ".jsx" extension
 
 export const DrinkSearch = () => {
-	const [searchField, setSearchField] = useState('milkshake');
+	const [searchField, setSearchField] = useState('milkshake')
 
 	return (
 		<>
