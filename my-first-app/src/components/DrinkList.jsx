64fb6dce69ed0fecba7a38cdf33aa3e290ca914a
Yorian Fransz <sydney-cook@outlook,com>
2024-06-11 21:22:06 +0200

    change some code

--- my-first-app/src/components/DrinkList.jsx
+++ my-first-app/src/components/DrinkList.jsx
@@ -1,10 +1,10 @@
 import { DrinkItem } from "./DrinkItem";
 
-export const DrinkList = ({ drinks }) => {
+export const DrinkList = ({ drinks, clickFn}) => {
     return (
         <>
             {drinks.map((drink) => (
-                <DrinkItem key={drink.id} drink={drink} />
+                <DrinkItem key={drink.id} drink={drink} clickFn={clickFn} />
             ))}
         </>
     );
