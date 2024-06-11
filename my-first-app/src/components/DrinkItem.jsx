a213a88800343aced8c21815ca16244b7e229ee5
Yorian Fransz <sydney-cook@outlook,com>
2024-06-11 18:07:18 +0200

    change some code

--- my-first-app/src/components/DrinkItem.jsx
+++ my-first-app/src/components/DrinkItem.jsx
@@ -3,7 +3,7 @@ import './DrinkItem.css';
 export const DrinkItem = ({ drink }) => {
   return (
     <div className="drink-item">
-      <img src={drink.imgUrl} alt={drink.alt} className="drink-image" />
+      <img src={drink.imgUrl} width={50} height={50} alt={drink.alt}/>
       <p>{drink.name}</p>
     </div>
   );
