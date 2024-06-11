37c8a75162684134e8eaf8bb0964d4c5b07e3e2d
Yorian Fransz <sydney-cook@outlook,com>
2024-06-11 17:06:29 +0200

    code

--- my-first-app/src/components/DrinkItem.jsx
+++ my-first-app/src/components/DrinkItem.jsx
@@ -1,10 +1,25 @@
-export const DrinkChoice = ({drink}) => {
-return (
-    <>
-    <h2>Your choice: {drink.name}</h2>
-    <img src={drink.imgUrl} alt={drink.alt} width="100px"
-    height="100px" />
-    <p>Your drink will be ready soon</p>
-    </>
-);
+// DrinkItem.js
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import './DrinkItem.css'; // Import your stylesheet
+
+const DrinkItem = ({ drink }) => {
+  return (
+    <div className="drink-item">
+      <img src={drink.imgUrl} alt={drink.alt} className="drink-image" />
+      <p className="drink-name">{drink.name}</p>
+    </div>
+  );
 };
+
+DrinkItem.propTypes = {
+  drink: PropTypes.shape({
+    ID: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    imgUrl: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default DrinkItem;
