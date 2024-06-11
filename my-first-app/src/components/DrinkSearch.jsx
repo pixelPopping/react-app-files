b9b3a1ac64c6615f3d20a02b6602c7292107fb6f
Yorian Fransz <sydney-cook@outlook,com>
2024-06-11 18:03:00 +0200

    change some code

--- my-first-app/src/components/DrinkSearch.jsx
+++ my-first-app/src/components/DrinkSearch.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react';
 import { TextInput } from './ui/TextInput'; // Remove the ".jsx" extension
+import { DrinkList } from './DrinkList';
+import { availableDrinks } from '../utils/data';
 
 export const DrinkSearch = () => {
-	const [searchField, setSearchField] = useState('milkshake')
+	const [searchField, setSearchField] = useState('test drink')
 
 	return (
 		<>
 			<label>Search for drinks:</label>
 			<TextInput />
-			<p>{searchField}</p>
+			<DrinkList drinks={availableDrinks}/>
 		</>
 	);
 };
