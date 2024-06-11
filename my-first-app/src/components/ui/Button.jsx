d108cf1c24f3c6050b4c226777ad59bc378b17ab
Yorian Fransz <sydney-cook@outlook,com>
2024-06-11 21:29:00 +0200

    change code

--- my-first-app/src/components/ui/Button.jsx
+++ my-first-app/src/components/ui/Button.jsx
@@ -1,4 +1,4 @@
 import   './Button.css';
 
-export const Button = ({text}) => <button className="button">{text}</button>;
+export const Button = ({text, clickFn}) =>( <button className="button" onClick={clickFn}> {text} </button>);
   
