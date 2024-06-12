6a4aa71ade3e578d66d650285112b70269983afb
Yorian Fransz <sydney-cook@outlook,com>
2024-06-12 18:38:11 +0200

    add some code

--- my-first-app/src/components/DrinkSearch.jsx
+++ my-first-app/src/components/DrinkSearch.jsx
@@ -4,13 +4,19 @@ import { DrinkList } from './DrinkList';
 import { availableDrinks } from '../utils/data';
 
 export const DrinkSearch = ({clickFn}) => {
-	const [searchField, setSearchField] = useState('test drink')
-
+	const [searchField, setSearchField] = useState("");
+    const handleChange = (event) => setSearchField(event.target.value);
+	const matchedDrinks = availableDrinks.filter((drink) => {
+	return drink.name.toLowerCase().includes(searchField.toLowerCase());
+	});
 	return (
 		<>
 			<label>Search for drinks:</label>
 			<TextInput />
-			<DrinkList clickFn={clickFn} drinks={availableDrinks}/>
+			<input type="text" value={searchField} onKeyDown={handleChange}/>
+			<DrinkList clickFn={clickFn} drinks={matchedDrinks}/>
+
 		</>
 	);
 };
+
