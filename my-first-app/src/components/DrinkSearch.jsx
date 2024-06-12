9ab8464326b242f2036882478fdea58ad5220fc6
Yorian Fransz <sydney-cook@outlook,com>
2024-06-12 19:04:18 +0200

    change sp,e code

--- my-first-app/src/components/DrinkSearch.jsx
+++ my-first-app/src/components/DrinkSearch.jsx
@@ -4,16 +4,19 @@ import { DrinkList } from './DrinkList';
 import { availableDrinks } from '../utils/data';
 
 export const DrinkSearch = ({clickFn}) => {
-	const [searchField, setSearchField] = useState("");
-    const handleChange = (event) => setSearchField(event.target.value);
+	const [searchField, setSearchField] = useState('');
+   
 	const matchedDrinks = availableDrinks.filter((drink) => {
 	return drink.name.toLowerCase().includes(searchField.toLowerCase());
 	});
-	return (
+
+	const handleChange = (event) => {
+		setSearchField(event.target.value);
+	};
+ return(
 		<>
 			<label>Search for drinks:</label>
-			<TextInput />
-			<input type="text" value={searchField} onKeyDown={handleChange}/>
+			<TextInput changeFn={handleChange} />
 			<DrinkList clickFn={clickFn} drinks={matchedDrinks}/>
 
 		</>
