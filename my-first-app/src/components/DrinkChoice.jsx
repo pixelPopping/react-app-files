eb6593f84447fe7815dad7c403145c42bd6f9601
Yorian Fransz <sydney-cook@outlook,com>
2024-06-13 18:17:17 +0200

    change code

--- my-first-app/src/components/DrinkChoice.jsx
+++ my-first-app/src/components/DrinkChoice.jsx
@@ -1,13 +1,13 @@
 import { Button } from "./ui/Button";
 
-export const DrinkChoice = ({drink, onClick}) => {
+export const DrinkChoice = ({drink, clickFn}) => {
 return (
     <>
     <h2>Your choice: {drink.name}</h2>
     <img src={drink.imgUrl} alt={drink.alt} width="100px"
     height="100px" />
     <p>Your drink will be ready soon</p>
-    <Button text={'Change selection'} onClick={() => onClick()} />
+    <Button text={'Change selection'} clickFn={() => clickFn()} />
     </>
 );
 };
