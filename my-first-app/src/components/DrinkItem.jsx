28f8a8eb29e877b809ed0ef592cecc3b7b4f9b29
Yorian Fransz <sydney-cook@outlook,com>
2024-06-13 16:47:23 +0200

    change code

--- my-first-app/src/components/DrinkItem.jsx
+++ my-first-app/src/components/DrinkItem.jsx
@@ -1,8 +1,8 @@
 import { Center, Image, Text} from '@chakra-ui/react'
 
-export const DrinkItem = ({ drink, onClick }) => {
+export const DrinkItem = ({ drink, onClick}) => {
   return (
-    <Center gap={8} cursor={'pointer'}  onClick={() => onClick()}>
+    <Center gap={8} cursor={'pointer'}  onClick={() => onClick(drink)}>
       <Image src={drink.imgUrl} w={50} h={50} alt={drink.alt}/>
       <Text>{drink.name}</Text>
     </Center>
