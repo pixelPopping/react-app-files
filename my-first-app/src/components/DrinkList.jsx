0b7a2a2a835a68c1479411465072fdb08a69be1e
Yorian Fransz <sydney-cook@outlook,com>
2024-06-13 20:55:15 +0200

    add some code

--- my-first-app/src/components/DrinkList.jsx
+++ my-first-app/src/components/DrinkList.jsx
@@ -1,15 +1,12 @@
-import { DrinkItem } from "./DrinkItem";
+import { Flex } from '@chakra-ui/react';
+import { DrinkItem } from './DrinkItem';
 
-export const DrinkList = ({ drinks, clickFn}) => {
-    
-    return (
-        <>
-            {drinks.map((drink) => (
-                <DrinkItem key={drink.id} drink={drink} clickFn={clickFn} />
-            ))}
-        </>
-    );
+export const DrinkList = ({ drinks, clickFn }) => {
+  return (
+    <Flex gap={16} w="50%" flexWrap="wrap" justify={'center'}>
+      {drinks.map((drink) => (
+        <DrinkItem key={drink.id} drink={drink} clickFn={clickFn} />
+      ))}
+    </Flex>
+  );
 };
-
-
-
