e712c5527ddb98b4e3eaff7889e99c8bd5657423
Yorian Fransz <sydney-cook@outlook,com>
2024-06-13 18:15:34 +0200

    add some code

--- my-first-app/src/components/DrinkList.jsx
+++ my-first-app/src/components/DrinkList.jsx
@@ -5,7 +5,7 @@ export const DrinkList = ({ drinks, clickFn}) => {
     return (
         <>
             {drinks.map((drink) => (
-                <DrinkItem key={drink.id} drink={drink} onClick={clickFn} />
+                <DrinkItem key={drink.id} drink={drink} clickFn={clickFn} />
             ))}
         </>
     );
